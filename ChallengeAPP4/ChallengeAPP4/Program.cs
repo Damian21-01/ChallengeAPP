3cdd1202ac6ba3bb9ea793d01ade46730f8e2f3e
Damian21-01 <135232408+Damian21-01@users.noreply.github.com>
2023-06-23 12:04:13 +0200

    Zadanie dzień 4

---
+++ ChallengeAPP4/ChallengeAPP4/Program.cs
@@ -0,0 +1,19 @@
+﻿var name = "Ewa";
+bool Female = false;
+var age = 33;
+
+if (Female == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (Female == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
