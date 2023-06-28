fe8c7eea36459ac03ac85b043e577c5fa94922fd
Damian21-01 <135232408+Damian21-01@users.noreply.github.com>
2023-06-28 16:31:29 +0200

    Zadanie Domowe- Dzień 7

--- ChallengeAPP/ChellengeAPP.Tests/EmployeedTests.cs
+++ ChallengeAPP/ChellengeAPP.Tests/EmployeedTests.cs
@@ -7,17 +7,22 @@ namespace ChellengeAPP
         {
             // arrange
             var employee = new Employee("Adam", "Lewandowski", 43);
+            var employee1 = new Employee("Anna", "Nowak", 50);
             employee.AddGrade(5);
             employee.AddGrade(6);
             employee.AddGrade(-1);
             employee.AddGrade(-1);
             employee.AddGrade(-7);
+            employee1.AddGrade(8);
+            employee1.AddGrade(2);
 
             // act
             var result = employee.Result;
-
+            var result1 = employee1.Result;
+            
             // assert 
             Assert.AreEqual(2, result);
+            Assert.AreEqual(10, result1);
         }
     }
 }
